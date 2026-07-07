5c3372be7781b3fcc2b7f648ba3ecb61dde3a3a3
kaziashik <ashik@apppay.tech>
2026-07-07 16:59:17 +0800

    rentnest_backend setup wtih prisma and express

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import config from "./config";
+
+// import { userRoutes } from "./modules/user/user.route";
+
+const app: Application = express();
+
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  })
+);
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// API
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, World!");
+});
+
+// app.use("/api/users", userRoutes);
+
+export default app;
